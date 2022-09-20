7cfad7ddf802fbae158b6c905fa9445e4937b62b
Jotch-96 <107109450+Plasma-Linux@users.noreply.github.com>
2022-09-20 21:02:50 +0900

    バグの修正

--- Firefox-Build.sh
+++ Firefox-Build.sh
@@ -4,9 +4,15 @@ sudo apt update && sudo apt full-upgrade -y
 
 sudo apt autoremove -y
 
-auso apt install -y wget
+sudo apt install -y wget
 
 echo "必ず/optと/usr/local/そして/usr/local/share/applicationsの管理権限を変更しておいてください!"
+
+sudo chmod +x /opt
+
+sudo chmod +x /usr/local/
+
+sudo chmod +x /usr/local/share/
 
 wget https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=ja&_gl=1*19cx649*_ga*MjU4MDIwOTMwLjE2NjA3MjI0Njg.*_ga_MQ7767QQQW*MTY2MTQ2ODY5OS4yLjEuMTY2MTQ2ODcxNC4wLjAuMA..
 
